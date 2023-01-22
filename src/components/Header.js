bb1b0beb2aea2eb4291db0902fe81a56a04e8c8e
Viraj <virajnawar4063@gmail.com>
2023-01-22 20:25:03 +0530

    Login btn

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,27 @@
-const Header = () => {
-    return (
-      <div className="header">
-        <img src="" alt="logo" />
-        <div className="nav">
-          <ul className="nav--links">
-            <li> Home </li> <li> About </li> <li> Contact </li> <li> Cart </li>{" "}
-          </ul>{" "}
-        </div>{" "}
-      </div>
-    );
-  };
+import { useState } from "react";
 
-  export default Header
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div className="header">
+      <img src="" alt="logo" />
+      <div className="nav">
+        <ul className="nav--links">
+          <li> Home </li>
+          <li> About </li>
+          <li> Contact </li>
+          <li> Cart </li>
+          <li>
+            {isLoggedIn ? (
+              <button id="logout__btn" onClick={() => setIsLoggedIn(false)}>Logout</button>
+            ) : (
+              <button id="login__btn" onClick={() => setIsLoggedIn(true)}>Login</button>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
