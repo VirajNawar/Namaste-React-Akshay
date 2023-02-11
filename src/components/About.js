169830eed94da1e250c8d8cf84903776cadb2120
Viraj <virajnawar4063@gmail.com>
2023-02-11 19:23:04 +0530

    class based (Chapter 8)

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,39 @@
-const About = () => {
-    return(
-        <div>
-            <h1>This is the About Us Page</h1>
-        </div>
-    )
+// const About = () => {
+// return(
+//     <div>
+//         <h1>This is the About Us Page</h1>
+//     </div>
+// )
+// }
+
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Constructor");
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(()=>{
+        console.log("About Interval");
+    },1000)
+    console.log("Component Did Mount");
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timer)
+    console.log("Will Unmount");
+  }
+
+  render() {
+    console.log("Render");
+    return (
+      <div>
+        <h1>This is the About Us Page</h1>
+      </div>
+    );
+  }
 }
 
-export default About
+export default About;
